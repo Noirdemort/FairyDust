cc384c397cf6b746980cc1c4da68148330620d82
Noirdemort <darkStillwater@protonmail.com>
2021-09-19 08:07:58 +0530

    Initial Commit

---
+++ Black Mist/Black_MistApp.swift
@@ -0,0 +1,17 @@
+//
+//  Black_MistApp.swift
+//  Black Mist
+//
+//  Created by Noirdemort on 19/09/21.
+//
+
+import SwiftUI
+
+@main
+struct Black_MistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Black Mist/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Black Mist
+//
+//  Created by Noirdemort on 19/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
